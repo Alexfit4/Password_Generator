16a69c82b1415f2ca0818cbb995ef52398941aa8
Alex Ashtiany <alex.ashtiany@gmail.com>
2020-11-21 19:39:21 -0500

    FINALLy fixed my script

--- script.js
+++ script.js
@@ -1,61 +1,82 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = "123456789";
+var lowerCase = "abcdefghijklmnopqrstuvwzrx";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChar = "!@#$%^&*+_-";
+
+function getRandom(max) {
+	return Math.floor(Math.random() * max);
+}
+
+function generatePassword(
+	Length,
+	hasNumbers,
+	hasUpperCase,
+	hasLowerCase,
+	hasSpecial
+) {
+	let password = "";
+	let selection = ["upper", "lower", "number", "special"];
+	while (password.length < Length) {
+		char = selection[getRandom(4)];
+		if (hasUpperCase && char == "upper") {
+			password += upperCase[getRandom(26)];
+		}
+		if (hasLowerCase && char == "lower") {
+			password += lowerCase[getRandom(26)];
+		}
+		if (hasNumbers && char == "number") {
+			password += numbers[getRandom(9)];
+		}
+		if (hasSpecial && char == "special") {
+			password += specialChar[getRandom(11)];
+		}
+	}
+
+	return password;
+}
 
 // Write password to the #password input
 function writePassword() {
-  
-    var charNumber = prompt("How many characters would you like to have 8-128");
-    while (isNaN(charNumber) || (charNumber > 128) || (charNumber < 8)) {
-      charNumber = prompt("Please enter a valid number")
-    }
+	var passwordLength = prompt(
+		"How many characters would you like to have 8-128"
+	);
+	while (isNaN(passwordLength) || passwordLength > 128 || passwordLength < 8) {
+		passwordLength = parseInt(prompt("Please enter a valid number"));
+	}
 
-    var includeNumbers = prompt("Would you like to include numbers? (Please enter Yes or No)");
-    includeNumbers.toLowerCase;
-    var yes= "yes";
-    var no= "no";
-    console.log(typeof includeNumbers);
+	var includeNumbers = confirm("Would you like to include numbers?");
+	console.log(includeNumbers);
 
-    while (includeNumbers != yes || includeNumbers != no) {
-      if(includeNumbers === yes) {
-        break;
-      } 
-      if(includeNumbers === no) {
-        break;
-      }
-      alert("Please input Yes or No")
-      includeNumbers = prompt("Would you like to include numbers? (Please enter Yes or No)");
-      includeNumbers.toLowerCase;
-    }
-    
-  
-  var password = generatePassword();  
-  var passwordText = document.querySelector("#password");
+	var upperCase = confirm("Would you like to include upper case letters?");
+	console.log(upperCase);
 
-  
+	var lowerCase = confirm("Would you like to include lower case letters?");
+	console.log(lowerCase);
 
-  
+	var specChar = confirm("Would you like to include special character?");
+	console.log(specChar);
 
-  passwordText.value = password;
+	var password = generatePassword(
+		passwordLength,
+		includeNumbers,
+		upperCase,
+		lowerCase,
+		specChar
+	);
+	var passwordText = document.querySelector("#password");
 
+	passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); {
-   
-    
+generateBtn.addEventListener("click", writePassword);
+{
 }
 
-// function message(){
-//     prompt("This is the message");
-// }
-
-var numbers = [1,2,3,4,5,6,7,8,9]
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
-var low = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
-var specialChar = ['!','@', '#', '$', '%', '^','&','*','-'] 
-
-console.log(numbers)
-console.log(upper)
-console.log(low)
-console.log(specialChar)
+console.log(numbers);
+console.log(upperCase);
+console.log(lowerCase);
+console.log(specialChar);
